3ef93f7f37fa26477109ca49cf6a56fb013da109
arik halperin <arik.halperin@s3code.com>
2020-11-10 14:49:51 +0200

    Adding writer queues

--- orkaudio/audiocaptureplugins/voip/SipParsers.cpp
+++ orkaudio/audiocaptureplugins/voip/SipParsers.cpp
@@ -156,7 +156,7 @@ bool TrySipBye(EthernetHeaderStruct* ethernetHeader, IpHeaderStruct* ipHeader, U
         __int64_t current_time = get_time_from_epoch_micros();
 
         LOG4CXX_INFO(s_sipPacketLog, "Stopping elvis");
-        SIPEventWriter::instance()->write_sip_event(info->from,info->to,info->m_callId,current_time,SipEventType::SIP_STOP)
+        SIPEventWriter::instance()->write_sip_event(info->m_from,info->m_to,info->m_callId,current_time,SipEventType::SIP_STOP)
     }
     return result;
 }
