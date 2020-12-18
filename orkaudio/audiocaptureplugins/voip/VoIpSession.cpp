a4962092ee671df8aeea295a6c96bf08fd0264aa
arik halperin <arik.halperin@s3code.com>
2020-12-18 10:46:10 +0200

    Fixing rtp timestamp

--- orkaudio/audiocaptureplugins/voip/VoIpSession.cpp
+++ orkaudio/audiocaptureplugins/voip/VoIpSession.cpp
@@ -1528,7 +1528,8 @@ bool VoIpSession::AddRtpPacket(RtpPacketInfoRef& rtpPacket)
 		AudioChunkRef chunk(new AudioChunk());
 		chunk->SetBuffer(rtpPacket->m_payload, details);
 		//g_audioChunkCallBack(chunk, m_capturePort);
-        AudioDataWriter::instance()->write_pcm_data(m_callId, channel, rtpPacket->m_payloadType, rtpPacket->m_seqNum ,rtpPacket->m_payloadSize, rtpPacket->m_payload, time(NULL) * 1000000);
+
+        AudioDataWriter::instance()->write_pcm_data(m_callId, channel, rtpPacket->m_payloadType, rtpPacket->m_seqNum ,rtpPacket->m_payloadSize, rtpPacket->m_payload, rtpPacket->m_timestamp);
 
 	}
   //  LOG4CXX_INFO(m_log, "AddRtpPacket 0xE1");
