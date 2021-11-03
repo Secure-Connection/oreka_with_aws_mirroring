d47bb82c5c1b07457e58713c579eee83d820fab1
EtK2000 <eytannovick@gmail.com>
2021-11-04 00:54:49 +0200

    (Hopeful) fix to no call audio

--- orkaudio/shared_queue/SIPEventWriter.h
+++ orkaudio/shared_queue/SIPEventWriter.h
@@ -11,9 +11,9 @@
 enum SipEventType : uint8_t { SIP_START, SIP_START_INVITE, SIP_STOP };
 
 typedef struct {
-    char caller[32];
-    char callee[32];
-    char session_id[64];
+    char caller[32]{};
+    char callee[32]{};
+    char session_id[64]{};
     uint64_t time_from_epoch_micros;
     SipEventType eventType;
 }SIPEvent;
