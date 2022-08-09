209077bc98019a69d4d52a7b4927e528447dcef6
arik halperin <arik.halperin@s3code.com>
2022-08-09 11:16:39 +0300

    debugging format

--- orkbasecxx/audiofile/LibSndFileFile.cpp
+++ orkbasecxx/audiofile/LibSndFileFile.cpp
@@ -63,7 +63,7 @@ void LibSndFileFile::Open(CStdString& filename, fileOpenModeEnum mode, bool ster
 		m_sampleRate = sampleRate;
 		m_fileInfo.samplerate = sampleRate;
 	}
-    char prompt[64]
+    char prompt[64];
     sprintf(prompt, "Fileinfo:%d:%d", m_fileInfo.format, m_fileInfo.channels);
 
 	if( (mode==WRITE) && !sf_format_check(&m_fileInfo))
