cc65d1d8cd62aa3f501ce2179ae1299554536e2f
arik halperin <arik.halperin@s3code.com>
2022-08-17 13:12:08 +0300

    Ready to deploy

--- orkaudio/audiocaptureplugins/voip/QMetrics.cpp
+++ orkaudio/audiocaptureplugins/voip/QMetrics.cpp
@@ -26,7 +26,7 @@ CStdString QMetrics::FinishCall(CStdString local_party, CStdString remote_party)
     msg.Format("Finished Call:%s %s", local_party, remote_party);
     log_to_packet_log(msg);
 
-    CStdString result = local_party+"at";
+    CStdString result = local_party;
 
 
     MutexSentinel mutexSentinel(m_mutex); // serialize access for competing pcap threads and tape finish
